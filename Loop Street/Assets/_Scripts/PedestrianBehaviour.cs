29cb9dd278e2ea0ef97a47945c287345df20903c
jazonthemazon <97111436+jazonthemazon@users.noreply.github.com>
2024-11-24 14:49:17 +0100

    Added animation changes and fixed perspective issues

--- Loop Street/Assets/_Scripts/PedestrianBehaviour.cs
+++ Loop Street/Assets/_Scripts/PedestrianBehaviour.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,8 +12,9 @@ public class PedestrianBehaviour : MonoBehaviour
     [SerializeField] private float _rotationIntensity;
 
     [SerializeField] private float _size;
-
     private SplineAnimate _splineAnimate;
+    private Animator _walkAnimator;
+    private Vector2 _previousPosition;
 
     private float _startingY;
 
@@ -22,21 +24,54 @@ public class PedestrianBehaviour : MonoBehaviour
         _splineAnimate = GetComponent<SplineAnimate>();
         _splineAnimate.Container = _splineToFollow;
         _splineAnimate.Play();
+        _walkAnimator = GetComponent<Animator>();
     }
 
     private void Start()
     {
-        _startingY = transform.position.y;
+        _previousPosition = transform.position;
     }
 
     private void Update()
     {
         // create depth illusion
         float currentY = transform.position.y;
-        transform.localScale = new Vector2(_size + (_startingY - currentY), _size + (_startingY - currentY));
+        transform.localScale = new Vector2(_size * (- currentY), _size * (- currentY));
 
         // rotate player slightly to the left and right of the screen
         float currentX = transform.position.x;
         transform.rotation = Quaternion.Euler(0, 0, -currentX * _rotationIntensity);
+
+        // change animation depending on movement direction
+        Vector2 currentPosition = transform.position;
+        Vector2 deltaPosition = currentPosition - _previousPosition;
+        deltaPosition.Normalize();
+
+        if (Mathf.Abs(deltaPosition.x) > Mathf.Abs(deltaPosition.y))
+        {
+            if (deltaPosition.x < 0)
+            {
+                _walkAnimator.SetTrigger("Trigger Side Walk");
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else if (deltaPosition.x > 0)
+            {
+                _walkAnimator.SetTrigger("Trigger Side Walk");
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+        else
+        {
+            if (deltaPosition.y < 0)
+            {
+                _walkAnimator.SetTrigger("Trigger Front Walk");
+            }
+            else if (deltaPosition.y > 0)
+            {
+                _walkAnimator.SetTrigger("Trigger Back Walk");
+            }
+        }
+
+        _previousPosition = currentPosition;
     }
 }
